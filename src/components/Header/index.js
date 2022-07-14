a893185d7dc17d3dd837782fcdcf6bcdf7ccd886
Damien Alves <alves.d24@laposte.net>
2022-07-14 11:55:41 +0200

    chore(global): init create-react-app

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="container-header">
+      <header className="header">
+        <p>Check your node</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
